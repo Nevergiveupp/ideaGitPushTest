2ed227e69bafbe6f3897beda150146b38b59c90c
prince <361785671@qq.com>
2019-04-26 19:33:47 +0800

    把本地工程添加到git

---
+++ git_test01/src/com/wzc/git/GitTest01.java
@@ -0,0 +1,12 @@
+package com.wzc.git;
+
+public class GitTest01 {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+        System.out.println("Hello Git");
+    }
+}
