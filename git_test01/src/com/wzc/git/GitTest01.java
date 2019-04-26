c0ce039b7e6a64eab0049853786a10a5d001d543
prince <361785671@qq.com>
2019-04-26 19:48:02 +0800

    第一次修改GitTest01.java

--- git_test01/src/com/wzc/git/GitTest01.java
+++ git_test01/src/com/wzc/git/GitTest01.java
@@ -8,11 +8,13 @@ public class GitTest01 {
         System.out.println("Hello Git");
         System.out.println("Hello Git");
         System.out.println("Hello Git");
+        //hello git
     }
 
     public void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 
 }
